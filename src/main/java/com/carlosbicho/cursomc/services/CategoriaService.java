24f25d17f8258ac3943a4b82ade9acbcf33e7c39
Carlos Bicho <carlosserranobicho@sapo.pt>
2020-01-26 17:11:43 +0000

    Criando repository e service para Categoria

---
+++ src/main/java/com/carlosbicho/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.carlosbicho.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlosbicho.cursomc.domain.Categoria;
+import com.carlosbicho.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);  
+		 return obj.orElse(null);
+	}
+}
