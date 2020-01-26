db394ca6c63ee148841037a94604214f41f826ba
Carlos Bicho <carlosserranobicho@sapo.pt>
2020-01-26 20:19:14 +0000

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/carlosbicho/cursomc/services/CategoriaService.java
+++ src/main/java/com/carlosbicho/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.carlosbicho.cursomc.domain.Categoria;
 import com.carlosbicho.cursomc.repositories.CategoriaRepository;
+import com.carlosbicho.cursomc.services.exeptions.ObjectsNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
-		 Optional<Categoria> obj = repo.findById(id);  
-		 return obj.orElse(null);
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectsNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
---
+++ src/main/java/com/carlosbicho/cursomc/services/exeptions/ObjectsNotFoundException.java
@@ -0,0 +1,14 @@
+package com.carlosbicho.cursomc.services.exeptions;
+
+public class ObjectsNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectsNotFoundException(String msg) {
+		super(msg);
+	}
+
+	public ObjectsNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
